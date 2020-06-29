856649e2e5e45c8f2ded00b38684f66178d14c88
Nikolai Leonov <strongardo@gmail.com>
2020-06-29 13:11:11 +0600

    Реализует обновление маркеров на карте

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -7,6 +7,7 @@ class Map extends PureComponent {
     super(props);
 
     this._mapRef = React.createRef();
+    // this._markers = [];
   }
 
   render() {
@@ -17,40 +18,55 @@ class Map extends PureComponent {
     const city = [52.38333, 4.9];
 
     const zoom = 12;
-    const map = leaflet.map(this._mapRef.current, {
+    this._map = leaflet.map(this._mapRef.current, {
       center: city,
       zoom,
       zoomControl: false,
       marker: true,
     });
-    map.setView(city, zoom);
+    this._map.setView(city, zoom);
 
-    const icon = leaflet.icon({
+    this._icon = leaflet.icon({
       iconUrl: `img/pin.svg`,
       iconSize: [30, 30],
     });
 
-    const iconActive = leaflet.icon({
+    this._iconActive = leaflet.icon({
       iconUrl: `img/pin-active.svg`,
       iconSize: [30, 30],
     });
 
-    this._layer = leaflet
+    leaflet
       .tileLayer(
           `https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`,
           {
             attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`,
           }
       )
-      .addTo(map);
+      .addTo(this._map);
+
+    this._addMarkers();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.activeMarker !== prevProps.activeMarker) {
+      this._map.removeLayer(this._layer);
+      this._addMarkers();
+    }
+  }
+
+  _addMarkers() {
+    this._markers = [];
 
     this.props.markers.forEach(({coordinates, id}) => {
-      leaflet
+      const marker = leaflet
         .marker(coordinates, {
-          icon: id === this.props.activeMarker ? iconActive : icon,
-        })
-        .addTo(map);
+          icon: id === this.props.activeMarker ? this._iconActive : this._icon,
+        });
+      this._markers.push(marker);
     });
+
+    this._layer = leaflet.layerGroup(this._markers).addTo(this._map);
   }
 }
 
