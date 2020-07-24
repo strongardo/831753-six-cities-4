649260fb3387c79517e3092586c2d6d92311c833
Nikolai Leonov <strongardo@gmail.com>
2020-07-24 15:00:16 +0600

    Вносит правки по замечаниям наставника

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -50,7 +50,7 @@ const OfferCard = (props) => {
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">{price}</b>
+            <b className="place-card__price-value">€{price}</b>
             <span className="place-card__price-text">/&nbsp;night</span>
           </div>
           <button
